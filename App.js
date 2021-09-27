59f6086fec2604cd54bce57987bdc38766617d95
Muhammad Umair Butt <muhammadumairbutt@192.168.10.5>
2021-09-27 11:18:09 +0500

    This is the first commit of this project.

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+const App= () => {
+  return (
+    <SafeAreaView style={{ flex:1 ,backgroundColor:'red'}}>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
